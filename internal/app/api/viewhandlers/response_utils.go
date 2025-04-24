8190606eed846ee1c8c896c761a553d1ad3aa785
Ayman <ayman.s.hamdoun@gmail.com>
2025-04-24 23:36:49 +0400

    add web server

---
+++ internal/app/api/viewhandlers/response_utils.go
@@ -0,0 +1,21 @@
+package apihandlers
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+)
+
+type errorResponse struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
+func _json(ctx context.Context, w http.ResponseWriter, structToWrite any) {
+	w.Header().Set("Content-Type", "application/json")
+
+	err := json.NewEncoder(w).Encode(structToWrite)
+	if err != nil {
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+	}
+}
