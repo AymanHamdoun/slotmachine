af1d4e2d585aa5c8fc1470387740884024896b91
Ayman <ayman.s.hamdoun@gmail.com>
2025-04-29 00:17:15 +0400

    install templ

--- internal/app/api/viewhandlers/user.go
+++ internal/app/api/viewhandlers/user.go
@@ -2,6 +2,7 @@ package viewhandlers
 
 import (
 	"context"
+	"go-backend/views"
 	"net/http"
 )
 
@@ -19,5 +20,7 @@ type createUserViewData struct {
 }
 
 func (h CreateUserHandler) Serve(ctx context.Context, input CreateUserInput, w http.ResponseWriter, r *http.Request) {
-	_view(ctx, w, r, "index.html", createUserViewData{})
+	_templ(w, r, views.DefaultLayout(
+		views.HeroSection("Hello World"),
+	))
 }
